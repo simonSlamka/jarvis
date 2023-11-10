8340612fa7d6ed63880b4d25e49b823bc319dedd
Simon Slamka <51794014+simonSlamka@users.noreply.github.com>
2023-11-10 05:54:36 +0100

    main done; pretty fkin cool

--- main.py
+++ main.py
@@ -1,69 +1,100 @@
 from openai import OpenAI
 import base64
+import os
+import sounddevice as sd
+import numpy as np
+import scipy.io.wavfile as wav
+import time
+
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
+
 import pygame
 from dotenv import load_dotenv
-import os
 from pathlib import Path
 
 load_dotenv()
 
 client = OpenAI(api_key=os.getenv("TOKEN"))
 
-import sounddevice as sd
-import numpy as np
-import scipy.io.wavfile as wav
-import time
+jarvis = client.beta.assistants.retrieve(
+    assistant_id="asst_pTUi0R7FgyGPpLKm5QpLhnOF"
+)
 
-fs = 44100  # Sample rate
+print(jarvis)
 
-print("Recording started. Press Ctrl+C to stop the recording.")
-myrecording = sd.InputStream(samplerate=fs, channels=2)
-myrecording.start()
-recording = []
+thread = client.beta.threads.create()
+
+fs = 44100
+
 try:
     while True:
-        data = myrecording.read(int(fs))
-        recording.append(data[0])
+
+        print("Listening ...")
+        myrecording = sd.InputStream(samplerate=fs, channels=2)
+        myrecording.start()
+        recording = []
+        try:
+            while True:
+                data = myrecording.read(int(fs))
+                recording.append(data[0])
+        except KeyboardInterrupt:
+            print("Done listening!")
+        finally:
+            myrecording.stop()
+            myrecording.close()
+            recording = np.concatenate(recording, axis=0)
+            wav.write('output.wav', fs, recording)
+
+        audio = open('output.wav', 'rb')
+
+        transcript = client.audio.transcriptions.create(
+            model="whisper-1",
+            file=audio,
+            response_format="text"
+        )
+
+        print(transcript)
+
+        msg = client.beta.threads.messages.create(
+            thread_id=thread.id,
+            role="user",
+            content=transcript
+        )
+
+        run = client.beta.threads.runs.create(
+            thread_id=thread.id,
+            assistant_id=jarvis.id,
+            instructions="Your response will be converted to speech, so avoid links and other non-speech content."
+        )
+
+        while run.status != "completed":
+            run = client.beta.threads.runs.retrieve(
+                thread_id=thread.id,
+                run_id=run.id
+            )
+            print("Stand by ...")
+            time.sleep(2)
+        msgs = client.beta.threads.messages.list(
+            thread_id=thread.id
+        )
+
+        recent = max(msgs.data, key=lambda msg: msg.created_at)
+        msg = recent.content[0].text.value
+        print(msg)
+
+        response = client.audio.speech.create(
+            model="tts-1",
+            voice="nova",
+            input=msg
+        )
+
+        response.stream_to_file("response.mp3")
+
+        pygame.mixer.init()
+        pygame.mixer.music.load('response.mp3')
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy() == True:
+            continue
+
 except KeyboardInterrupt:
-    print("Recording stopped.")
-finally:
-    myrecording.stop()
-    myrecording.close()
-    recording = np.concatenate(recording, axis=0)
-    wav.write('output.wav', fs, recording)  # Save as WAV file
-
-audio = open('output.wav', 'rb')
-
-transcript = client.audio.transcriptions.create(
-    model="whisper-1",
-    file=audio,
-    response_format="text"
-)
-
-print(transcript)
-
-returned = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "You're Hannah. You act like a human at all costs and in all situations."},
-        {"role": "user", "content": transcript}
-    ]
-)
-
-resp = returned.choices[0].message.content
-print(resp)
-
-response = client.audio.speech.create(
-    model="tts-1",
-    voice="nova",
-    input=resp
-)
-
-response.stream_to_file("response.mp3")
-
-pygame.mixer.init()
-pygame.mixer.music.load('response.mp3')
-pygame.mixer.music.play()
-while pygame.mixer.music.get_busy() == True:
-    continue
-
+    print("Goodbye!")
